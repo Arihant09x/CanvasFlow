1131973fed78a2b10f161f64c2b0de814da2b4e0
Arihant09x <arihantc677@gmail.com>
2025-07-24 23:16:56 +0530

    Added https  Domain name

--- apps/web/service/api.ts
+++ apps/web/service/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://canvasflow.devvault.site/api/v1";
+const BASE_URL = "https://canvasflow.devvault.site/";
 const apiClient = axios.create({
   baseURL: BASE_URL,
   headers: {
